b87e5e7b47ef547b2f865d5876ecf80721d1a6d1
Prajjwal Datir <prajjwaldatir0072000@gmail.com>
2021-06-27 02:40:15 +0530

    added `play`
    sends an audio using just a search term

---
+++ src/commands/Media/play.ts
@@ -0,0 +1,31 @@
+import { MessageType } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
+import yts from 'yt-search'
+import YT from '../../lib/YT'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'play',
+            description: '🎵 play a song with just search term!',
+            category: 'media',
+            aliases: ['music'],
+            usage: `${client.config.prefix}play [term]`,
+            dm: true,
+            baseXp: 30
+        })
+    }
+
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+        if (!joined) return void M.reply('Please provide a search term')
+        const term = joined.trim()
+        const { videos } = await yts(term)
+        if (!videos || videos.length <= 0) return void M.reply(`No Matching videos found for the term : *${term}*`)
+        const audio = new YT(videos[0].url, 'audio')
+        M.reply(await audio.getBuffer(), MessageType.audio).catch(() => M.reply('An error occurred...'))
+
+    }
+}
