a1f1bd4c9181025a63fb0483f129ba607a976c6b
Prajjwal Datir <prajjwaldatir0072000@gmail.com>
2021-06-27 22:20:38 +0530

    Added `play` command with preview

--- src/commands/Media/play.ts
+++ src/commands/Media/play.ts
@@ -25,7 +25,19 @@ export default class Command extends BaseCommand {
         const { videos } = await yts(term)
         if (!videos || videos.length <= 0) return void M.reply(`No Matching videos found for the term : *${term}*`)
         const audio = new YT(videos[0].url, 'audio')
+        if(!audio.url) return
         M.reply('Sending...')
-        M.reply(await audio.getBuffer(), MessageType.audio).catch(() => M.reply('An error occurred...'))
+        this.client.sendMessage(M.from,await audio.getBuffer() , MessageType.audio, {
+            quoted: M.WAMessage,
+            contextInfo: {
+                externalAdReply: {
+                    title: videos[0].title.substr(0,30),
+                    body: "via Kaoi && Void",
+                    mediaType: 1,
+                    thumbnailUrl: `https://i.ytimg.com/vi/${audio.id}/hqdefault.jpg`, 
+                    mediaUrl: audio.url
+                }
+            }
+        })
     }
 }
