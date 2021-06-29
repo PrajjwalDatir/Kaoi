8b3c166f27354d20ed5478ae5b64c6b35447b977
Prajjwal Datir <prajjwaldatir0072000@gmail.com>
2021-06-29 13:39:03 +0530

    changed body in play

--- src/commands/Media/play.ts
+++ src/commands/Media/play.ts
@@ -32,7 +32,7 @@ export default class Command extends BaseCommand {
             contextInfo: {
                 externalAdReply: {
                     title: videos[0].title.substr(0,30),
-                    body: "via Kaoi && Void",
+                    body: `Link: ${videos[0].url}`,
                     mediaType: 1,
                     thumbnailUrl: `https://i.ytimg.com/vi/${audio.id}/hqdefault.jpg`, 
                     mediaUrl: audio.url
