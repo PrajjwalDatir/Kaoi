36da57a6e9f7b4aa9805295a082d379aa6d5ca22
Prajjwal Datir <prajjwaldatir0072000@gmail.com>
2021-07-01 01:07:48 +0530

    Added `kaoi`

--- src/commands/Misc/Kaoi.ts
+++ src/commands/Misc/Kaoi.ts
@@ -3,23 +3,22 @@ import { join } from 'path'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
-import { IPackage, ISimplifiedMessage } from '../../typings'
+import { ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'void',
+            command: 'kaoi',
             description: 'Displays the info',
             category: 'misc',
-            usage: `${client.config.prefix}void`,
+            usage: `${client.config.prefix}kaoi`,
             dm: true
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         // eslint-disable-next-line @typescript-eslint/no-var-requires
-        const pkg: IPackage = require(join(__dirname, '..', '..', '..', 'package.json'))
-        const image = this.client.assets.get('void')
+        const image = `https://avatars.githubusercontent.com/u/46681482?v=4`
         if (!image) return void null
         return void M.reply(
             image,
