611e893263baa7041b6e7747fedd98576cac9922
Prajjwal Datir <prajjwaldatir0072000@gmail.com>
2021-07-01 02:55:42 +0530

    Handled Non-MODS trying to use BAN-UNBAN

--- src/commands/Dev/Ban.ts
+++ src/commands/Dev/Ban.ts
@@ -14,10 +14,10 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        if (!this.client.config.mods?.includes(M.sender.jid)) return void null
+        if (!this.client.config.mods?.includes(M.sender.jid)) return void M.reply('❌ Only available to MODS')
         const immortals = [M.sender, this.client.user.jid]
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
-        if (!M.mentioned.length || !M.mentioned[0]) return void M.reply('Please mention the user whom you want to ban')
+        if (!M.mentioned.length || !M.mentioned[0]) return void M.reply('Mention the user whom you want to ban')
         let text = '*STATE*\n\n'
         for (const user of M.mentioned) {
             if (immortals.includes(user)) continue
