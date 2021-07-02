90d29556e2588e5af9c1958a0627c0c75aa56228
Gen0s_kun <82636931+AB1RZ9@users.noreply.github.com>
2021-07-02 09:42:40 +0530

    Update and rename Kaoi.ts to Saber.ts

--- src/commands/Misc/Saber.ts
+++ src/commands/Misc/Saber.ts
@@ -17,7 +17,7 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        return void M.reply(`👾 *kaoi* 👾\n\n🍀 *Description:* Maintained Fork of WhatsApp Botto Void\n\n🌐 *URL:* https://github.com/PrajjwalDatir/Kaoi#readme\n\n📂 *Repository:* https://github.com/PrajjwalDatir/Kaoi`
+        return void M.reply(`🐦 *Saber* 🐦\n\n🍀 *Description:* Maintained Fork of WhatsApp Botto Void\n\n🌐 *URL:* https://github.com/AB1RZ9/Saber-Pendragon#readme\n\n📂 *Repository:* https://github.com/AB1RZ9/Saber-Pendragon`
         ).catch((reason: any) =>
             M.reply(`an error occupered, Reason: ${reason}`))
     }
