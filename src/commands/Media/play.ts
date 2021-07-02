04a714fe5308be37ed217f01562b42cccedea5ec
Gen0s_kun <82636931+AB1RZ9@users.noreply.github.com>
2021-07-02 09:38:47 +0530

    Update play.ts

--- src/commands/Media/play.ts
+++ src/commands/Media/play.ts
@@ -26,7 +26,7 @@ export default class Command extends BaseCommand {
         if (!videos || videos.length <= 0) return void M.reply(`⚓ No Matching videos found for the term : *${term}*`)
         const audio = new YT(videos[0].url, 'audio')
         if (!audio.url) return
-        M.reply('👾 Sending...')
+        M.reply('Bringing your song Oni chan!')
         this.client
             .sendMessage(M.from, await audio.getBuffer(), MessageType.audio, {
                 quoted: M.WAMessage,
