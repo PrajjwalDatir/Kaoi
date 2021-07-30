51b1811ddbb5984d7daa4c658add0e7f20632727
Gen0s_kun <82636931+AB1RZ9@users.noreply.github.com>
2021-07-30 21:42:37 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -2,6 +2,7 @@ import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
+6import request from '../../lib/request'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
