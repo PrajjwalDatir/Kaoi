1e936ebb952f8521302c80ab8615fc7e73705e6d
Gen0s_kun <82636931+AB1RZ9@users.noreply.github.com>
2021-07-30 18:14:31 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -29,6 +29,7 @@ export default class Command extends BaseCommand {
                     categories[info.config.category].push(info)
                 }
             }
+M.reply( buffer.request( 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQix-YZrimsPF_cv5gtvrmNWDTuY2DEbtNpLg&usqp=CAU'), messageType.image, undefined, undefined, `text`)
             let text = `✨ *Saber's Command List* ✨\n\n`
             const keys = Object.keys(categories)
             for (const key of keys)
