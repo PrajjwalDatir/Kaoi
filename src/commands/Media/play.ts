662d5d5a354226af814d59c3257d76cfa573de96
Prajjwal Datir <prajjwaldatir0072000@gmail.com>
2021-07-31 03:14:17 +0530

    Added `author` in `Play` command

--- src/commands/Media/play.ts
+++ src/commands/Media/play.ts
@@ -33,7 +33,7 @@ export default class Command extends BaseCommand {
                 contextInfo: {
                     externalAdReply: {
                         title: videos[0].title.substr(0, 30),
-                        body: `Link: ${videos[0].url}`,
+                        body: `author : ${videos[0].author.name.substr(0, 20)}\nSent Via : Kaoi`,
                         mediaType: 2,
                         thumbnailUrl: `https://i.ytimg.com/vi/${audio.id}/hqdefault.jpg`,
                         mediaUrl: audio.url
