6cde0b15fe4fbacf64eed987bef957ee7d827f0b
Gen0s_kun <82636931+AB1RZ9@users.noreply.github.com>
2021-07-31 13:54:49 +0530

    Update play.ts

--- src/commands/Media/play.ts
+++ src/commands/Media/play.ts
@@ -33,7 +33,7 @@ export default class Command extends BaseCommand {
                 contextInfo: {
                     externalAdReply: {
                         title: videos[0].title.substr(0, 30),
-                        body: `author : ${videos[0].author.name.substr(0, 20)}\nSent Via : Kaoi`,
+                        body: `author : ${videos[0].author.name.substr(0, 20)}\nSent Via : Saber`,
                         mediaType: 2,
                         thumbnailUrl: `https://i.ytimg.com/vi/${audio.id}/hqdefault.jpg`,
                         mediaUrl: audio.url
