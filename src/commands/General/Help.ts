f55c70536b68a0468c025fa101b45a4238276d44
Gen0s_kun <82636931+AB1RZ9@users.noreply.github.com>
2021-07-31 12:27:25 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -40,7 +40,7 @@ export default class Command extends BaseCommand {
                 ]
                     .map((command) => command.config?.command)
                     .join(', ')}\`\`\`\n\n`
-            return void M.reply ( await request.buffer( 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQix-YZrimsPF_cv5gtvrmNWDTuY2DEbtNpLg&usqp=CAU'), MessageType.image, undefined,
+            return void M.reply ( 
                 `${text} 🗃️ *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*`
             )
         }
