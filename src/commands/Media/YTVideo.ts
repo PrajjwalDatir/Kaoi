7575258fcf9ecd7ccb94a73f0a4846cd032cbaa3
Prajjwal Datir <prajjwaldatir0072000@gmail.com>
2021-08-05 13:53:50 +0530

    Updated `ytdl` Reduced `play` & `yta` output size

--- src/commands/Media/YTVideo.ts
+++ src/commands/Media/YTVideo.ts
@@ -26,7 +26,7 @@ export default class Command extends BaseCommand {
         M.reply('👾 sending...')
         if (Number(videoDetails.lengthSeconds) > 1800)
             return void M.reply('⚓ Cannot Download videos longer than 30 Minutes')
-        M.reply(await video.getBuffer(), MessageType.video).catch((reason: any) =>
+        M.reply(await video.getBuffer(), MessageType.video).catch((reason: Error) =>
             M.reply(`❌ an error occupered, Reason: ${reason}`)
         )
     }
