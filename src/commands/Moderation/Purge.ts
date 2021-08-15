28c56398fbb2fa9ba862a9e1e0032cede5d1301f
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2021-08-15 09:37:29 +0530

    Fix typo

--- src/commands/Moderation/Purge.ts
+++ src/commands/Moderation/Purge.ts
@@ -25,7 +25,7 @@ export default class Command extends BaseCommand {
         if (!this.purgeSet.has(M.groupMetadata?.id || '')) {
             this.addToPurge(M.groupMetadata?.id || '')
             return void M.reply(
-                "Are you sure? This will remove everyone from the group chat. Use this comman again if you'd like to proceed"
+                "Are you sure? This will remove everyone from the group chat. Use this command again if you'd like to proceed"
             )
         }
         M.groupMetadata.participants.map(async (user) => {
