ec767d2d92cd540201fe09c82574d6b53e9c3ad6
Prajjwal Datir <46681482+PrajjwalDatir@users.noreply.github.com>
2021-08-18 21:16:52 +0530

    Updated Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -22,16 +22,43 @@ export default class Command extends BaseCommand {
             buffer = await this.client.downloadMediaMessage(M.quoted.message)
         if (M.WAMessage.message?.imageMessage || M.WAMessage.message?.videoMessage)
             buffer = await this.client.downloadMediaMessage(M.WAMessage)
-        if (!buffer) return void M.reply(`You didn't provide any Image/Video to convert`)
-        parsedArgs.flags.forEach((flag) => (parsedArgs.joined = parsedArgs.joined.replace(flag, '')))
-        const pack = parsedArgs.joined.split('|')
-        const crop:boolean = parsedArgs.flags.includes('--stretch') || parsedArgs.flags.includes('--s')
+        if (!buffer) return void M.reply(this.client.responses.get('invalid-sticker-param', M.response))
+        flags.forEach((flag) => (joined = joined.replace(flag, '')))
+        const pack = joined.split('|')
+        const categories = (() => {
+            const categories = flags.reduce((categories, flag) => {
+                switch (flag) {
+                    case '--angry':
+                        categories.push('💢')
+                        break
+                    case '--love':
+                        categories.push('💕')
+                        break
+                    case '--sad':
+                        categories.push('😭')
+                        break
+                    case '--happy':
+                        categories.push('😂')
+                        break
+                    case '--greet':
+                        categories.push('👋')
+                        break
+                    case '--celebrate':
+                        categories.push('🎊')
+                        break
+                }
+                return categories
+            }, new Array<Categories>())
+            categories.length = 2
+            if (!categories[0]) categories.push('❤', '🌹')
+            return categories
+        })()
         const sticker = new Sticker(buffer, {
+            categories,
             pack: pack[1] || '👾 𝐇𝐚𝐧𝐝𝐜𝐫𝐚𝐟𝐭𝐞𝐝 𝐅𝐨𝐫 𝐘𝐨𝐮 ',
             author: pack[2] || '𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭𝐭𝐨 𝐊𝐚𝐨𝐢 👾',
-            crop: crop
+            type: flags.includes('--crop') || flags.includes('--c') ? 'crop' : flags.includes('--stretch') || flags.includes('--s') ? 'default' : 'full'
         })
-        await sticker.build()
-        await M.reply(await sticker.get(), MessageType.sticker, Mimetype.webp)
+        await M.reply(await sticker.build(), MessageType.sticker, Mimetype.webp)
     }
 }
