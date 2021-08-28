dd89cf7c2500398b94dd01e7edc218921bc5062e
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2021-08-28 18:04:44 +0530

    Made ffmpeg based features unavailble

--- src/commands/Fun/Trigger.ts
+++ src/commands/Fun/Trigger.ts
@@ -19,6 +19,7 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
+        return void M.reply(`Animated sticker feature is currently unavailable.`)
         const getImage = async (image: string | Buffer, timeout = 15) => {
             const img = await Canvas.loadImage(image)
             const GIF = new GIFEncoder(256, 310)
