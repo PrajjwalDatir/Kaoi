a78d25eef6d8e77bf6718cbf2ea19205fb273356
Restyled.io <commits@restyled.io>
2021-09-02 11:11:34 +0000

    Restyled by whitespace

--- src/commands/General/did_you_mean.ts
+++ src/commands/General/did_you_mean.ts
@@ -10,7 +10,7 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: '',
-         
+
 
         })
     }
