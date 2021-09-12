a6195f81a26676b651c54049de88bf1a5b74b7a7
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2021-09-12 23:07:23 +0530

    Added extra `categories`

--- src/typings/command.d.ts
+++ src/typings/command.d.ts
@@ -19,4 +19,4 @@ export interface ICommand {
     }
 }
 
-export type TCategory = 'general' | 'moderation' | 'misc' | 'media' | 'utils' | 'dev' | 'category' | 'fun'
+export type TCategory = 'general' | 'moderation' | 'misc' | 'media' | 'utils' | 'dev' | 'category' | 'fun' | 'reactions' | 'reactions2'
