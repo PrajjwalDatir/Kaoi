36a19b020be5ae8ea99613a4083471ebf4ec461b
jayjayops <85323007+jayjayops@users.noreply.github.com>
2021-09-19 16:30:01 +0100

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -29,7 +29,7 @@ export default class Command extends BaseCommand {
                     categories[info.config.category].push(info)
                 }
             }
-            let text = `👾 *Kaoi's Command List* 👾\n\n`
+            let text = `*❗Ethan's Command List* 🔎\n\n`
             const keys = Object.keys(categories)
             for (const key of keys)
                 text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[
@@ -38,7 +38,7 @@ export default class Command extends BaseCommand {
                     .map((command) => command.config?.command)
                     .join(', ')}\`\`\`\n\n`
             return void M.reply(
-                `${text} 🗃️ *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*`
+                `${text} 🚧 *Note: Use ${this.client.config.prefix}help & command_name to view the command info*`
             )
         }
         const key = parsedArgs.joined.toLowerCase()
@@ -46,17 +46,17 @@ export default class Command extends BaseCommand {
         if (!command) return void M.reply(`No Command of Alias Found | "${key}"`)
         const state = await this.client.DB.disabledcommands.findOne({ command: command.config.command })
         M.reply(
-            `🎫 *Command:* ${this.client.util.capitalize(command.config?.command)}\n🎗️ *Status:* ${
+            `🗒️ *Command:* ${this.client.util.capitalize(command.config?.command)}\n🏷️ *Status:* ${
                 state ? 'Disabled' : 'Available'
-            }\n🀄 *Category:* ${this.client.util.capitalize(command.config?.category || '')}${
+            }\n🗄️ *Category:* ${this.client.util.capitalize(command.config?.category || '')}${
                 command.config.aliases
-                    ? `\n🍥 *Aliases:* ${command.config.aliases.map(this.client.util.capitalize).join(', ')}`
+                    ? `\n🖇️ *Aliases:* ${command.config.aliases.map(this.client.util.capitalize).join(', ')}`
                     : ''
-            }\n🃏 *Group Only:* ${this.client.util.capitalize(
+            }\n👥 *Group Only:* ${this.client.util.capitalize(
                 JSON.stringify(!command.config.dm ?? true)
-            )}\n🎀 *Usage:* ${command.config?.usage || ''}\n\n🔖 *Description:* ${command.config?.description || ''}`
+            )}\n⚒️ *Usage:* ${command.config?.usage || ''}\n\n📍 *Description:* ${command.config?.description || ''}`
         )
     }
 
-    emojis = ['👨‍💻', '🌀', '🎴', '🔮', '👑', '🎈', '⚙️', '⚙️', '🍀']
+    emojis = ['👽', '🌀', '🚨', '🔮', '💡', '🎈', '♟️', '🎭', '⚙️']
 }
