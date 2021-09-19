dea42eb1f5cff9d6bc16af1d73a3a7f148974b8b
jayjayops <85323007+jayjayops@users.noreply.github.com>
2021-09-19 20:25:25 +0100

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -7,7 +7,7 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'mods',
-            description: "Displays the Moderators' contact info",
+            description: "Displays the Owner's contact info",
             category: 'general',
             usage: `${client.config.prefix}mods`,
             aliases: ['moderators', 'mod', 'owner']
@@ -17,14 +17,14 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!this.client.config.mods || !this.client.config.mods[0]) return void M.reply('*[UNMODERATED]*')
         const filteredMap = this.client.config.mods.map((mod) => this.client.getContact(mod)).filter((user) => user)
-        let text = '🍥 *Moderators* 🍥\n\n'
+        let text = ' *👾CREATED BY  ৫⃟🔥➤᭄᭄̊̊̊̊࿓࿔𝑱𝒂𝒚𝑱𝒂𝒚۝⃟̥̩̩̩̥̩̥͚̮🫂* \n\n'
         filteredMap.forEach(
             (user, index) =>
-                (text += `#${index + 1}\n🎫 *Username: ${
-                    user.notify || user.vname || user.name || 'null'
-                }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
+                (text += `#${index + 1}\n *ᴏᴡɴᴇʀ ➪:* ${
+                    user.notify || user.vname || user.name || 'ᴊᴀʏᴊᴀʏ'
+                }\n☎️ *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
-        text += `\nTo deploy your own Bot or To support Kaoi👾\nVisit : https://github.com/PrajjwalDatir/Kaoi `
+        text += `\n*ʙᴏᴛ ɴᴀᴍᴇ ➪* ❗ᴇᴛʜᴀɴ-ʙᴏᴛ\n*ᴘʀᴇғɪx ➪* !\n*ʙᴏᴛ ɴᴏ☎️ ➪* wa.me/+2349026336891\n\n*sᴛᴀᴛᴜs ➪* ᴏɴʟɪɴᴇ ɴᴏᴡ𒊹︎︎︎`
         return void M.reply(text)
     }
 }
