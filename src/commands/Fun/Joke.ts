d72638139eb419b0ae3e7d9941226f02ec43294c
Nakshatra <87998363+Nakshatra4762@users.noreply.github.com>
2021-09-21 12:37:48 +0530

    Create Joke.ts

---
+++ src/commands/Fun/Joke.ts
@@ -0,0 +1,30 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'joke',
+            description: 'random jokes.',
+            aliases: ['j'],
+            category: 'fun',
+            usage: `${client.config.prefix}joke`
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        await axios
+            .get(`https://v2.jokeapi.dev/joke/Any`)
+            .then((response) => {
+                // console.log(response);
+                const text = `📝 *Catagory:* ${response.data.category}\n\n*🎃 Joke:* ${response.data.setup}\n\n*💡 Answer:* ${response.data.delivery}`
+                M.reply(text)
+            })
+            .catch((err) => {
+                M.reply(`🔍 Error: ${err}`)
+            })
+    }
+}
