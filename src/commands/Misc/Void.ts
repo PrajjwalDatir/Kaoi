e27d8685b9a63d12a9c2fa9d3f78bfca18ecd458
Hiroto <83062954+Hiroto77@users.noreply.github.com>
2021-09-22 13:47:33 +0530

    Update Void.ts

--- src/commands/Misc/Void.ts
+++ src/commands/Misc/Void.ts
@@ -25,7 +25,7 @@ export default class Command extends BaseCommand {
             MessageType.image,
             undefined,
             undefined,
-            `🖤 *Void* 🖤\n\n🍀 *Description: ${pkg.description}*\n\n🌐 *URL: ${pkg.homepage}*\n\n📂`
+            `🖤 *Void* 🖤\n\n🍀 *Description: ${pkg.description}*\n\n🌐 *URL: ${pkg.homepage}*\n`
         )
     }
 }
