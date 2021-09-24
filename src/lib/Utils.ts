39a95177c1b88a88dd8b9b5d8a07dfa5b4af74b3
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2021-09-24 18:33:01 +0530

    adds `GIFBufferToVideoBuffer`

--- src/lib/Utils.ts
+++ src/lib/Utils.ts
@@ -1,8 +1,26 @@
 import { readdirSync, statSync } from 'fs'
 import { join } from 'path'
 import getUrls from 'get-urls'
+import { exec } from 'child_process'
+import { readFile, unlink, writeFile } from 'fs/promises'
+import { tmpdir } from 'os'
+import { promisify } from 'util'
 
 export default class {
+    
+    exec = promisify(exec)
+    GIFBufferToVideoBuffer = async (image: Buffer): Promise<Buffer> => {
+        const filename = `${tmpdir()}/${Math.random().toString(36)}`
+        await writeFile(`${filename}.gif`, image)
+        await this.exec(
+            `ffmpeg -f gif -i ${filename}.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" ${filename}.mp4`
+        )
+        const buffer = await readFile(`${filename}.mp4`)
+        Promise.all([unlink(`${filename}.mp4`), unlink(`${filename}.gif`)])
+        return buffer
+    }
+
+
     readdirRecursive = (directory: string): string[] => {
         const results: string[] = []
 
