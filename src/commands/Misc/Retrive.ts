6d7218a07836abbeaabbfd6ce0eebab5f82160d2
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2021-09-24 18:33:28 +0530

    Video msg retrieval

--- src/commands/Misc/Retrive.ts
+++ src/commands/Misc/Retrive.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { MessageType } from '@adiwajshing/baileys'
+import { MessageType, proto, WAMessage } from '@adiwajshing/baileys'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
@@ -17,17 +17,22 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        if (!M.quoted) return void (await M.reply(`🔖 Quote the message you want to retrive`))
-        if (!(M?.quoted?.message?.message as any)?.viewOnceMessage?.message?.imageMessage)
-            return void M.reply(
-                `Tag the "viewOnceMessage" that you want to retrive\nIf you're already tagging it, try updating Bot's WhatsApp`
-            )
+        if (!M.quoted) return void (await M.reply(`Quote the "viewOnceMessage" you want to retrieve`))
+        if (
+            !(M?.quoted?.message?.message as proto.Message)?.viewOnceMessage?.message?.videoMessage &&
+            !(M.quoted.message?.message as proto.Message).viewOnceMessage?.message?.imageMessage
+        )
+            return void M.reply('Quote the "viewOnceMessage" that you want to retrieve')
         return void M.reply(
-            await this.client.downloadMediaMessage((M.quoted.message?.message as any).viewOnceMessage),
-            MessageType.image,
+            await this.client.downloadMediaMessage(
+                (M.quoted.message?.message as proto.Message).viewOnceMessage as WAMessage
+            ),
+            MessageType[
+                (M.quoted.message?.message as proto.Message).viewOnceMessage?.message?.imageMessage ? 'image' : 'video'
+            ],
             undefined,
             undefined,
-            'Here you go. - feature via *void*'
+            'Hippity Hoppity, this message is now public property...'
         )
     }
-}
+    }
