e7b3ba66e0c85b96e081f9ab3b874e1a3650abe0
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2021-10-06 23:33:07 +0530

    Can't ban the MODS and BOT

--- src/commands/Dev/Ban.ts
+++ src/commands/Dev/Ban.ts
@@ -15,22 +15,36 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!this.client.config.mods?.includes(M.sender.jid)) return void M.reply('❌ Only available to MODS')
-        const immortals = [M.sender, this.client.user.jid]
+        // Can't ban the BOT, MODS
+        const immortals = [M.sender.jid, this.client.user.jid, ...this.client.config.mods]
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
         if (!M.mentioned.length || !M.mentioned[0]) return void M.reply('Mention the user whom you want to ban')
         let text = '*STATE*\n\n'
+        // declare tagged as (string | undefined) []
+        // const tagged : (string | undefined)[] = []
         for (const user of M.mentioned) {
-            if (immortals.includes(user)) continue
+            if (immortals.includes(user)) {
+                // tagged.push(user)
+                text += `🟨 @${user.split('@')[0]} is an immortal, can't be banned\n`
+                continue
+            }
             const data = await this.client.getUser(user)
-            const info = this.client.getContact(user)
-            const username = info.notify || info.vname || info.name || user.split('@')[0]
+            // const info = this.client.getContact(user)
+            // const username = info.notify || info.vname || info.name || user.split('@')[0]
+            // const username = user.split('@')[0]
             if (data?.ban) {
-                text += `🟨 ${username}: Already Banned\n`
+                text += `🟨 @${user.split('@')[0]}: Already Banned\n`
                 continue
             }
             await this.client.banUser(user)
-            text += `🟥 ${username}: Banned\n`
+            text += `🟥 @${user.split('@')[0]}: Banned\n`
         }
-        M.reply(text)
+        await M.reply(
+            `${text}`,
+            undefined,
+            undefined,
+            // undefined
+        [...M.mentioned, M.sender.jid]
+        )
     }
 }
