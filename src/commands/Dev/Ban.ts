e272db77e7b7325d93c09e966c96faac296494de
Restyled.io <commits@restyled.io>
2021-10-06 22:38:49 +0000

    Restyled by whitespace

--- src/commands/Dev/Ban.ts
+++ src/commands/Dev/Ban.ts
@@ -15,10 +15,10 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        const immortals = this.client.config.mods 
-        ? [M.sender.jid, this.client.user.jid, ... this.client.config.mods] 
+        const immortals = this.client.config.mods
+        ? [M.sender.jid, this.client.user.jid, ... this.client.config.mods]
         : [M.sender.jid, this.client.user.jid]
-        
+
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
         if (!M.mentioned.length || !M.mentioned[0]) return void M.reply('Mention the user whom you want to ban')
         let text = '*STATE*\n\n'
