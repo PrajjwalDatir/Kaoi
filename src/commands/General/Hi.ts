a92bc7c3f12f3d244b7dc2e8425cddca5bff065e
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2021-10-10 01:28:08 +0530

    Sends back Hi + Used to test Bot

---
+++ src/commands/General/Hi.ts
@@ -0,0 +1,20 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'hi',
+            description: 'Generally used to check if bot is Up',
+            category: 'general',
+            usage: `${client.config.prefix}hi`,
+            baseXp : 0,
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        return void (await M.reply(`👾 Hello ${M.sender.username}!`))
+    }
+}
