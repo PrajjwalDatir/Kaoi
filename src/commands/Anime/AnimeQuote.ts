781284cce2c8140b62d1138ef145074eb3fd1889
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2021-10-10 01:15:46 +0530

    `AnimeQuote` Gives you an Anime Quote

---
+++ src/commands/Anime/AnimeQuote.ts
@@ -0,0 +1,31 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'animequote',
+            description: 'random anime quote.',
+            aliases: ['aq'],
+            category: 'anime',
+            usage: `${client.config.prefix}animequote`,
+            baseXp: 10
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        await axios
+            .get(`https://animechan.vercel.app/api/random`)
+            .then((response) => {
+                // console.log(response);
+                const text = `⛩ *Anime:* ${response.data.anime}\n\n*🎎 Character:* ${response.data.character}\n\n*✏ Quote:* ${response.data.quote}`
+                M.reply(text)
+            })
+            .catch((err) => {
+                M.reply(`🔍 Error: ${err}`)
+            })
+    }
+}
