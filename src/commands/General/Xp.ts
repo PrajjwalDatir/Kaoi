d2be7921b235cc255bec08429fbfc2af675b3821
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2021-10-10 01:28:52 +0530

    |Xp| Displays the user's xp|

--- src/commands/General/Xp.ts
+++ src/commands/General/Xp.ts
@@ -10,19 +10,20 @@ export default class Command extends BaseCommand {
             description: "Displays User's Xp 🌟",
             category: 'general',
             usage: `${client.config.prefix}xp (@tag)`,
-            aliases: ['exp']
+            aliases: ['exp'],
+            baseXp : 10,
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
         const user = M.mentioned[0] ? M.mentioned[0] : M.sender.jid
-        let username = user === M.sender.jid ? M.sender.username : 'Person'
+        let username = user === M.sender.jid ? M.sender.username : 'Your'
         if (!username) {
             // const contact = this.client.getContact(user)
             // username = contact.notify || contact.vname || contact.name || user.split('@')[0]
             username = user.split('@')[0]
         }
-        return void (await M.reply(`${username}'s XP: ${(await this.client.getUser(user)).Xp || 0}`))
+        return void (await M.reply(`${username} XP: ${(await this.client.getUser(user)).Xp || 0}`))
     }
 }
