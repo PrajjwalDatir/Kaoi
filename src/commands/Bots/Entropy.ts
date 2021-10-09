b085b382ed6d5219603cdf8d7c5d32bcbfb41f28
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2021-10-10 01:18:25 +0530

    |Entropy| Gives you info about the Entropy Bot|

---
+++ src/commands/Bots/Entropy.ts
@@ -0,0 +1,22 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'entropy',
+            description: 'Displays the info',
+            category: 'bots',
+            usage: `${client.config.prefix}entropy`,
+            baseXp : 100,
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        return void M.reply(
+            `👾 *Entropy* 👾\n\n🍀 *Description:* The Only WhatsApp Bot With Multi-Device Support\n\n🌐 *URL:* https://github.com/Synthesized-Infinity/Whatsapp-Botto-Entropy\n`
+        ).catch((reason: Error) => M.reply(`an error occurred, Reason: ${reason}`))
+    }
+}
