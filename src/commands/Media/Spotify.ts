b4cd5297095fc3727fbaeaa8023cd38be55a3c69
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2021-10-10 01:31:56 +0530

    |Spotify|  Downloads songs from Spotify|
    Fixed typos and added xp

--- src/commands/Media/Spotify.ts
+++ src/commands/Media/Spotify.ts
@@ -33,7 +33,9 @@ export default class Command extends BaseCommand {
             undefined,
             undefined,
             caption
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         ).catch((reason: any) => M.reply(`❌ an error occurred, Reason: ${reason}`))
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         M.reply(await track.getAudio(), MessageType.audio).catch((reason: any) =>
             M.reply(`❌ an error occurred, Reason: ${reason}`)
         )
