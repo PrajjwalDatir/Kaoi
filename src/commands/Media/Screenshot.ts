0bdc0bd33f25772c85ad0ba9f75ff24095a4813b
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-10-13 19:53:48 +0530

    Screenshot ✅

---
+++ src/commands/Media/Screenshot.ts
@@ -0,0 +1,33 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+import request from '../../lib/request'
+import { MessageType } from '@adiwajshing/baileys'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'screenshot',
+            aliases: ['ss', 'ssweb'],
+            description: 'Gives you the screenshot of the given url. ',
+            category: 'utils',
+            usage: `${client.config.prefix}screenshot [url]`,
+            baseXp: 30
+        })
+    }
+    
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+        if (!joined) return void (await M.reply(`Please provide the url`))
+        const url = joined.trim()
+        return void M.reply( await request.buffer(`https://shot.screenshotapi.net/screenshot?&url=${url}&full_page=true&fresh=true&output=image&file_type=png&wait_for_event=load`),
+        MessageType.image,
+                    undefined,
+                    undefined,
+                    `🌟 Here you go.\n`,
+                    undefined
+                ).catch((reason: any) =>
+            M.reply(`✖ An error occurred. Please try again later. ${reason}`))
+    }
+}
