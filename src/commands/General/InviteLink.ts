6059bc0684466357a4fba252bada3ab22c65e6eb
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2021-10-15 14:54:52 +0530

    add `invite` cmd

---
+++ src/commands/General/InviteLink.ts
@@ -0,0 +1,32 @@
+import { MessageType } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'invitelink',
+            aliases: ['invite'],
+            description: 'Get the group invite link',
+            category: 'general',
+            usage: `${client.config.prefix}invite`,
+            baseXp: 10,
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        // check if Bot is the admin
+        if (!M.groupMetadata?.admins?.includes(this.client.user.jid)) return void M.reply(`I'm not an admin of this group.`)
+        if ((await this.client.getGroupData(M.from)).invitelink) {
+            const code = await this.client.groupInviteCode(M.from).catch(() => { return void M.reply('Could not get the invite link')})
+            // if(!code) return void M.reply('Could not get invite code')
+            await this.client.sendMessage(M.sender.jid, `Invite link: https://chat.whatsapp.com/${code}`, MessageType.text)
+            return void M.reply('Sent you the Group Link in personal message')
+        }
+        else {
+            return void M.reply(`Command not enabled by the admin.\nUse *${this.client.config.prefix}act invitelink* to enable it`)
+        }
+    }
+}
