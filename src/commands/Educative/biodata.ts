0d18fb058bebd1bc87acd7e788e21e201d279683
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-10-19 09:49:53 +0530

    Country Code Added.
    Returns Country Code when the sender gives Invalid Code.

--- src/commands/Educative/biodata.ts
+++ src/commands/Educative/biodata.ts
@@ -10,15 +10,16 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'biodata',
-            description: `Generate random biodata for given country code`,
+            description: `Generate random biodata from the given country code.`,
             aliases: ['biodata', 'bio'],
             category: 'educative',
-            usage: `${client.config.prefix}biodata [name]`,
+            usage: `${client.config.prefix}biodata [country_code]`,
             baseXp: 50
         })
     }
 
     run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+        const country = await axios.get(`https://api.abirhasan.wtf/bioDataGenerator`)
         if (!joined) return void (await M.reply(`Please provide the country code to generate data like for India give _in_ `))
         const biodata = joined.trim()
         await axios
@@ -28,7 +29,7 @@ export default class Command extends BaseCommand {
                 M.reply(text)
             })
             .catch((err) => {
-                M.reply(`Sorry, couldn't find country *${biodata}*\n📝 *Note:* Capital Letters do not work here.`)
+                M.reply(`Sorry, couldn't find country *${biodata}*\n📒 *List of Biodata:*\n${country.data.CountryCodesList}\n\n📝 *Note:* Capital letters don't work here.`)
             })
     }
 }
