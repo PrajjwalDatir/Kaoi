abe2442e4500f5e0e29d9bc4bda5cf4152c74a8b
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-02 22:35:54 +0530

    Create Create steal.ts

---
+++ src/commands/Fun/Create steal.ts
@@ -0,0 +1,22 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import { Sticker } from 'wa-sticker-formatter'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'steal',
+            description: 'Steals stickers',
+            category: 'utils',
+            usage: `${client.config.prefix}steal [(as caption | tag)[video | image]]`,
+            baseXp: 30
+        })
+    }
+
+    run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
+        return void M.reply("This command is under development")
+    }
+}
