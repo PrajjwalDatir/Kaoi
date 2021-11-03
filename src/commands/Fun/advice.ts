37bdbb580ed32e17ad26f6f31196651735e06e43
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-03 19:39:08 +0530

    Advice.ts
     An advice command in fun category

---
+++ src/commands/Fun/advice.ts
@@ -0,0 +1,31 @@
+import​ ​{​ ​MessageType​ ​}​ ​from​ ​'@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'advice',
+            description: 'Gives you random advice.',
+            category: 'fun',
+            usage: `${client.config.prefix}why`,
+            baseXp: 30
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        await axios
+            .get(`https://api.adviceslip.com/advice`)
+            .then((response) => {
+                // console.log(response);
+                const text = `📝 *Advice for you🔖:* ${response.data.advice}`
+                M.reply(text)
+            })
+            .catch((err) => {
+                M.reply(`🔍 Error: ${err}`)
+            })
+    }
+}
