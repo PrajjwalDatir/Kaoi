e01f58cede9be195ae272ade039780b57348d410
Raj6544 <92223399+Raj6544@users.noreply.github.com>
2021-11-11 08:01:13 +0530

    Update EventHandler.ts

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -23,7 +23,7 @@ export default class EventHandler {
                   .map((jid) => `@${jid.split('@')[0]}`)
                   .join(', ')}*`
             : event.action === 'remove'
-            ? `*@${event.participants[0].split('@')[0]}* Bye👋. We will miss you ✨`
+            ? `*@${event.participants[0].split('@')[0]}* Bye 👋`
             : `*@${event.participants[0].split('@')[0]}* got ${this.client.util.capitalize(event.action)}d${
                   event.actor ? ` by *@${event.actor.split('@')[0]}*` : ''
               }`
