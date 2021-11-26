6c1648573da16e18329c13fe08ff4bc6aaada95f
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-26 12:29:13 +0530

    Create Meme.ts

---
+++ src/commands/Fun/Meme.ts
@@ -0,0 +1,49 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+import request from '../../lib/request'
+import { MessageType } from '@adiwajshing/baileys'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'meme',
+            description: 'Will send you a Meme.',
+            aliases: ['LOL'],
+            category: 'fun',
+            usage: `${client.config.prefix}meme`
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        // fetch result of https://zxbott.herokuapp.com/husbu from the API using axios
+        const { data } = await axios.get('https://some-random-api.ml/meme')
+        const buffer = await request.buffer(data.image).catch((e) => {
+            return void M.reply(e.message)
+        })
+        while (true) {
+            try {
+                M.reply(
+                    buffer || 'Could not fetch image. Please try again later',
+                    MessageType.image,
+                    undefined,
+                    undefined,
+                    `LOL 😂`,
+                    undefined
+                ).catch((e) => {
+                    console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
+                    // console.log('Failed')
+                    M.reply(`Try again or use the link. Here's the URL: ${data.image}`)
+                })
+                break
+            } catch (e) {
+                // console.log('Failed2')
+                M.reply(`Try again or use the link. Here's the URL : ${data.image}`)
+                console.log(`This error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
+            }
+        }
+        return void null
+    }
+}
