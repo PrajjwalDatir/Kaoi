4105c2c5f1213584164f308fe793d1bf27d9272b
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-27 13:24:49 +0530

    Create Image.ts

---
+++ src/commands/Media/Image.ts
@@ -0,0 +1,51 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+import request from '../../lib/request'
+import { MessageType } from '@adiwajshing/baileys'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'image',
+            description: 'Will search img from the given term',
+            aliases: ['img'],
+            category: 'media',
+            usage: `${client.config.prefix}img`
+        })
+    }
+
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+        if (!joined) return void (await M.reply(`Please give a term to search`));
+        const search: any = joined.trim();
+        const term: string = search;
+        const { data } = await axios.get('https://imsea.herokuapp.com/api/1?q=${term}');
+        const buffer = await request.buffer(data.results).catch((e) => {
+            return void M.reply(e.message)
+        })
+        while (true) {
+            try {
+                M.reply(
+                    buffer || 'Could not fetch image. Please try again later',
+                    MessageType.image,
+                    undefined,
+                    undefined,
+                    `HERE YOU GO ✨`,
+                    undefined
+                ).catch((e) => {
+                    console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
+                    // console.log('Failed')
+                    M.reply(`Try again or use the link. Here's the URL: ${data.results}`)
+                })
+                break
+            } catch (e) {
+                // console.log('Failed2')
+                M.reply(`Try again or use the link. Here's the URL : ${data.results}`)
+                console.log(`This error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
+            }
+        }
+        return void null
+    }
+} 
