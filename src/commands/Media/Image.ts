bb174706bbfc952a4be403daed57aa5de687f6ab
Liton124 <92647972+Liton124@users.noreply.github.com>
2021-11-27 13:33:20 +0530

    Update Image.ts

--- src/commands/Media/Image.ts
+++ src/commands/Media/Image.ts
@@ -1,7 +1,7 @@
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
-import { ISimplifiedMessage } from '../../typings'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
 import axios from 'axios'
 import request from '../../lib/request'
 import { MessageType } from '@adiwajshing/baileys'
