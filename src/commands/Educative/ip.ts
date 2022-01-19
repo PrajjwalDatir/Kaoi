a8bd96152ec86f374ff77fd0757a8aef0bb435ab
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2022-01-19 11:55:41 +0530

    finally fixed

--- src/commands/Educative/ip.ts
+++ src/commands/Educative/ip.ts
@@ -2,7 +2,7 @@ import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { IParsedArgs, ISimplifiedMessage } from '../../typings'
-import axios from 'axios' // BRRRRRRRRRRRRRRRRRRRRRRRU
+import axios from 'axios'
 import request from '../../lib/request'
 import { MessageType, Mimetype } from '@adiwajshing/baileys'
 
