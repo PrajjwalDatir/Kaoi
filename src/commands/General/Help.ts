8a2a70c86d266d12d82bfe441192c2da809337cc
Cybaries <65446412+Cybaries@users.noreply.github.com>
2022-03-11 18:57:28 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -29,7 +29,7 @@ export default class Command extends BaseCommand {
                     categories[info.config.category].push(info)
                 }
             }
-            let text = `👾 *Kaoi's Command List* 👾\n\n`
+            let text = `👾 *Void's Command List* 👾\n\n`
             const keys = Object.keys(categories).sort((a, b) => a.localeCompare(b))
             for (const key of keys)
                 text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[
